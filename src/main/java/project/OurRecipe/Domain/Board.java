cc89ab8d88363f4af1a12a5dfd2e9f7181c2afef
Jinwoo, Nam <66236978+wrewre46@users.noreply.github.com>
2022-04-12 23:23:49 +0900

    Add BootStrap and Board domain

---
+++ src/main/java/project/OurRecipe/Domain/Board.java
@@ -0,0 +1,17 @@
+package project.OurRecipe.Domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter@Setter
+public class Board {
+    private int UserID;
+    private int BoardID;
+    private String BoardTitle;
+    private String BoardContent;
+    private boolean BoardAvailable;
+    private LocalDateTime WriteTime;
+
+}
---
+++ src/main/java/project/OurRecipe/Repository/BoardRepository.java
@@ -0,0 +1,4 @@
+package project.OurRecipe.Repository;
+
+public class BoardRepository {
+}

88d7aef8dbeb838e17849484ca0d83e7dfc39746
Jinwoo, Nam <66236978+wrewre46@users.noreply.github.com>
2022-05-15 15:25:41 +0900

    Validation V1

--- src/main/java/project/OurRecipe/Controller/LoginController.java
+++ src/main/java/project/OurRecipe/Controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import project.OurRecipe.Config.Auth.PrincipalDetails;
 import project.OurRecipe.Domain.Member;
 import project.OurRecipe.Repository.MemberRepository;
-//import project.OurRecipe.Validation.MemberValidation;
+import project.OurRecipe.Validation.MemberValidation;
 
 import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
@@ -32,7 +32,7 @@ public class LoginController {
 	
 	@Autowired private MemberRepository memberRepository;
 	@Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
-	//@Autowired private MemberValidation memberValidation;
+	@Autowired private MemberValidation memberValidation;
 	@GetMapping("/test/login")
 	public @ResponseBody String testLogin(Authentication authentication,
 			@AuthenticationPrincipal PrincipalDetails userDetails) {
@@ -79,16 +79,17 @@ public class LoginController {
 	}
 
 	@GetMapping("/join")
-	public String joinForm() {
+	public String joinForm(Model model) {
+		model.addAttribute("member",new Member());
 		return "login/joinForm";
 	}
 	@PostMapping("/join")
-	public String join(@ModelAttribute Member member) {
-//		memberValidation.validate(member,bindingResult);
-//		if(bindingResult.hasErrors()){
-//			log.info("error={}", bindingResult);
-//			return "/join";
-//		}
+	public String join(@Validated @ModelAttribute Member member, BindingResult bindingResult, Model model) {
+		memberValidation.validate(member,bindingResult);
+		if(bindingResult.hasErrors()){
+			log.info("error={}", bindingResult);
+			return "login/joinForm";
+		}
 		member.setID(memberRepository.MemberCount()+1);
 		member.setRole("ROLE_USER");
 		member.setMemberCreateDate(Date.valueOf(LocalDate.now()));
